b9af63b077f3e7d8cd60d3ca5c0efe2aec6a1868
Medha0909 <103240973+Medha0909@users.noreply.github.com>
2024-04-10 08:38:13 -0700

    Update Login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -17,7 +17,7 @@ function Login (props){
       let navigate=useNavigate();
       const handleSubmit=async (e)=>{
       e.preventDefault();
-      const response = await fetch("/getAll/reg/login",{
+      const response = await fetch("http://localhost:1000/reg/login",{
         method:'POST',
         headers:{
           'Content-Type': 'application/json'
