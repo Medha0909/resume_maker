2e8e09dd035d15d05999e8f19d4f5c1cee1d5295
Medha0909 <103240973+Medha0909@users.noreply.github.com>
2024-04-14 01:08:43 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const port=process.env.PORT || 10000;
+const port=process.env.PORT || 5000;
 const express = require('express');
 const app = express();
 const cors = require("cors");
